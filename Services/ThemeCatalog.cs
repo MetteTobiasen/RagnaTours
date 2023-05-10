a600a1bff0fc9e6f98dda43d944a83e9bebca3af
MetteTobiasen <tobiasen_1@hotmail.com>
2023-05-10 12:51:17 +0200

    Tilføjet metoder, properties og constructor

--- Services/ThemeCatalog.cs
+++ Services/ThemeCatalog.cs
@@ -1,6 +1,37 @@
-﻿namespace RagnaTours.Services
+﻿using RagnaTours.Interfaces;
+using RagnaTours.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RagnaTours.Services
+
 {
-    public class ThemeCatalog
+    public class ThemeCatalog : IThemeRepository
     {
+        private Dictionary<int, Theme> themes { get;}
+
+        public ThemeCatalog()
+        {
+            themes = new Dictionary<int, Theme>();  
+        }
+
+        public Dictionary<int, Theme> AllThemes()
+        {
+            return themes;  
+        }
+
+        public void AddTheme(Theme theme)
+        {
+            if (!themes.Keys.Contains(theme.Id))
+            {
+                themes.Add(theme.Id, theme);
+            }
+        }
+
+
+
+
     }
 }
