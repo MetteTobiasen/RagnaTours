f732ea554441f02a3f270e6f8e8cbb09276599d1
khengelbreth <khengelbreth@gmail.com>
2023-05-11 10:13:21 +0200

    A little somthing in Exhibition class

--- Models/Exhibition.cs
+++ Models/Exhibition.cs
@@ -1,6 +1,19 @@
-﻿namespace RagnaTours.Models
+﻿using RagnaTours.Services;
+
+namespace RagnaTours.Models
 {
     public class Exhibition
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+
+        private Dictionary<int, Exhibition> exhibition { get; }
+
+        //public ExhibitionCatalog() 
+        //{ 
+        //    exhibition = new Dictionary<int, Exhibition>();
+        //}
     }
 }
