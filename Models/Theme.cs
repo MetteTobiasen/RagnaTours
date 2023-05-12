5ad2338ce7d6f17715d0140de6a8719e34a581b7
MetteTobiasen <tobiasen_1@hotmail.com>
2023-05-12 11:47:59 +0200

    Færdiggjort ThemeCatalog Tilføjet Theme

--- Models/Theme.cs
+++ Models/Theme.cs
@@ -1,6 +1,11 @@
 ﻿namespace RagnaTours.Models
 {
     public class Theme
-    {
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Description { get; set; }
+
+        public string ImageName { get; set; }   
     }
 }
