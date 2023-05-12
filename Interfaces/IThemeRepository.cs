4f69fc83ea59151afa459df3f7508bf4d1d7ab28
khengelbreth <khengelbreth@gmail.com>
2023-05-12 11:16:40 +0200

    Interfaces.

--- Interfaces/IThemeRepository.cs
+++ Interfaces/IThemeRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace RagnaTours.Interfaces
+﻿using RagnaTours.Models;
+
+namespace RagnaTours.Interfaces
 {
     public interface IThemeRepository
     {
+        Dictionary<int, Theme> AllThemes();
+        void AddTheme(Theme theme);
+        void RemoveTheme(Theme theme);
+        void UpdateTheme(Theme theme);
+        Dictionary<int, Theme> SearchTheme(string criteria);
     }
 }
