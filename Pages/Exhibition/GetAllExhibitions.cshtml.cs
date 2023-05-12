9170fc96b7f7c5345fcbf085e2506e96064f41c7
richardx <richardx10@gmail.com>
2023-05-12 11:41:12 +0200

    Lavet html delen til Exhibition listen

--- Pages/Exhibition/GetAllExhibitions.cshtml.cs
+++ Pages/Exhibition/GetAllExhibitions.cshtml.cs
@@ -1,19 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RagnaTours.Interfaces;
+using RagnaTours.Models;
 
 namespace RagnaTours.Pages
 {
-    public class PrivacyModel : PageModel
+    public class GetAllExhibitionsModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        private IExhibitionRepository catalog;
+        public GetAllExhibitionsModel(IExhibitionRepository repository)
         {
-            _logger = logger;
+            catalog = repository;
         }
+        public Dictionary<int, Exhibition> Exhibitions { get; private set; }
 
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string FilterCriteria { get; set; }
+
+        public IActionResult OnGet()
         {
+            Exhibitions = catalog.AllExhibitions();
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Exhibitions = catalog.FilterPizza(FilterCriteria);
+            }
+
+            return Page();
         }
     }
 }
