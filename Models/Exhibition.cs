560514519ee7746fe2a40ca0609c1b4da2a1e32f
richardx <richardx10@gmail.com>
2023-05-30 10:22:00 +0200

    Krav

--- Models/Exhibition.cs
+++ Models/Exhibition.cs
@@ -1,13 +1,19 @@
 ﻿using RagnaTours.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace RagnaTours.Models
 {
     public class Exhibition
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string ImageName { get; set; }
+        [Required]
         public string ThemeName { get; set; }
 
     }
--- Models/Theme.cs
+++ Models/Theme.cs
@@ -1,11 +1,16 @@
-﻿namespace RagnaTours.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RagnaTours.Models
 {
     public class Theme
-    {   
+    {
+        [Required]
         public int Id { get; set; }
-        public string Name { get; set; }    
+        [Required]
+        public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
-
+        [Required]
         public string ImageName { get; set; }   
     }
 }
